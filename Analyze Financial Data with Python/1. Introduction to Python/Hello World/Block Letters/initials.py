735cb2a2e0be7dea5727fd21f09c5984f24a8b39
Eleanorcdavies <81331495+Eleanorcdavies@users.noreply.github.com>
2021-07-13 20:38:08 -0400

    Create initials.py

---
+++ Analyze Financial Data with Python/1. Introduction to Python/Hello World/Block Letters/initials.py
@@ -0,0 +1,6 @@
+# I am learning German
+string_E = 'EEEEE\nE\nE\nEEEEE\nE\nE\nEEEEEE'
+# print(string_E)
+string_D = 'DDDD \nD   D\nD   D\nD   D\nD   D\nD   D\nDDDD'
+# print(string_D)
+print(string_E +'\n' + string_D)
