afb61f1cde9af7ba8fc8c441f5ee4c7aab4fbcb1
Eleanorcdavies <81331495+Eleanorcdavies@users.noreply.github.com>
2021-07-13 20:31:48 -0400

    Create Tree.java
    Eleanor's Work

---
+++ Learn Java/1. Hello World/Planting a Tree/Tree.java
@@ -0,0 +1,17 @@
+// Define your tree class in this file!
+
+public class Tree{
+ public static void main(String[] args) {
+   //This will introdruce us and then print a tree to the screen
+   System.out.println("Hi, I am Eleanor and I am desperate to get a job!");
+   System.out.println("I will plant a tree today.");
+    System.out.println("    *  ");
+    System.out.println("   *** ");
+    System.out.println("  ***** ");
+    System.out.println(" *******  ");
+    System.out.println("    *  ");
+    System.out.println("    *  ");
+  }
+
+
+}
